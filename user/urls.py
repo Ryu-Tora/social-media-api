c85f199d435603b7390cfc932368e6d980360893
Adam Halka <galkaadam4@gmail.com>
2026-01-10 10:04:13 +0100

    custom login with email and added custom UserManager

---
+++ user/urls.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from user.views import CreateUserView, LoginUserView, ManageUserView
+
+app_name = "user"
+
+urlpatterns = [
+    path(
+        "register/",
+        CreateUserView.as_view(),
+        name="create",
+    ),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+]
